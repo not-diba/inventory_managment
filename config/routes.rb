daaa91924bf8fe44f1d1e49420b6db2bbf2d1084
Elvis Madiba <38106494+not-diba@users.noreply.github.com>
2023-03-15 18:28:55 +0300

    Nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
-  root "departments#index"
+  devise_for :administrators
+  root 'departments#index'
 
-  resources :departments
+  resources :departments do
+    resources :labs do
+      resources :equipment
+    end
+    resources :rooms do
+      resources :equipment
+    end
+    resources :administrators do
+      resources :roles
+    end
+  end
 end
