ba34d6d14b7736820595909c6116542429b74e8d
Elvis Madiba <38106494+not-diba@users.noreply.github.com>
2023-03-15 18:25:36 +0300

    Adding Labs

--- app/controllers/labs_controller.rb
+++ app/controllers/labs_controller.rb
@@ -1,24 +1,30 @@
 # frozen_string_literal: true
 
 class LabsController < ApplicationController
-    def index
-    end
+  def index; end
 
-    def show
-    end
+  def show
+    @department = Department.find(params[:department_id])
+    @lab = @department.labs.find(params[:id])
+  end
 
-    def new
-    end
+  def new; end
 
-    def create
-    end
+  def create
+    @department = Department.find(params[:department_id])
+    @lab = @department.labs.create(labs_params)
+    redirect_to department_path(@department)
+  end
 
-    def edit
-    end
+  def edit; end
 
-    def update
-    end
+  def update; end
 
-    def delete
-    end
+  def delete; end
+
+  private
+
+  def labs_params
+    params.require(:lab).permit(:lab_name)
+  end
 end
--- app/models/lab.rb
+++ app/models/lab.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Lab < ApplicationRecord
   belongs_to :department
   has_many :equipment
