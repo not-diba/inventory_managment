3150c90f1ada2f073e0f742d3c6bb6b27e8488d6
Elvis Madiba <38106494+not-diba@users.noreply.github.com>
2023-03-15 18:27:00 +0300

    Adding Rooms

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,24 +1,27 @@
 # frozen_string_literal: true
 
 class RoomsController < ApplicationController
-    def index
-    end
+  def index; end
 
-    def show
-    end
+  def show; end
 
-    def new
-    end
+  def new; end
 
-    def create
-    end
+  def create
+    @department = Department.find(params[:department_id])
+    @room = @department.rooms.create(rooms_params)
+    redirect_to department_path(@department)
+  end
 
-    def edit
-    end
+  def edit; end
 
-    def update
-    end
+  def update; end
 
-    def delete
-    end
+  def delete; end
+
+  private
+
+  def rooms_params
+    params.require(:room).permit(:room_name)
+  end
 end
