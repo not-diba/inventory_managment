d0f1153bbc097216d5e19c77e9708595e9fd809d
Elvis Madiba <38106494+not-diba@users.noreply.github.com>
2023-04-07 16:04:14 +0300

    routing to the equipment controller using shallow nesting

--- app/controllers/equipment_controller.rb
+++ app/controllers/equipment_controller.rb
@@ -1,24 +1,31 @@
 # frozen_string_literal: true
 
 class EquipmentController < ApplicationController
-    def index
-    end
+  def index; end
 
-    def show
-    end
+  def show
+    @equipment = Equipment.find(params[:id])
+  end
 
-    def new
-    end
+  def new; end
 
-    def create
-    end
+  def create
+    @lab = Lab.find(params[:lab_id])
+    params[:equipment].merge!({ room_id: @lab.id })
+    @equipment = @lab.equipment.create(equipment_params)
+    redirect_to lab_path(@lab)
+  end
 
-    def edit
-    end
+  def edit; end
 
-    def update
-    end
+  def update; end
 
-    def delete
-    end
+  def delete; end
+
+  private
+
+  def equipment_params
+    params.require(:equipment).permit(:equipment_name, :make, :serial_number, :model_number, :status, :remarks,
+                                      :room_id)
+  end
 end
