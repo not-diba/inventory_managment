d47ed28e247e94e6742f6e0e3ea6ab135f42845e
Elvis Madiba <38106494+not-diba@users.noreply.github.com>
2023-04-08 22:04:46 +0300

    adding administrator login and sign up

--- app/controllers/administrators_controller.rb
+++ app/controllers/administrators_controller.rb
@@ -1,2 +1,25 @@
+# frozen_string_literal: true
+
 class AdministratorsController < ApplicationController
+  # before_action :authorize
+  def new
+    @administrator = Administrator.new
+  end
+
+  def create
+    @administrator = Administrator.new(administrator_params)
+    if @administrator.save
+      session[:user_id] = @administrator.id
+      redirect_to root_url, notice: 'Thank you for signing up!'
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def administrator_params
+    # TODO: change department_id to name using a drop down menu
+    params.require(:administrator).permit(:name, :email, :role, :department_id, :password, :password_confirmation)
+  end
 end
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+class SessionsController < ApplicationController
+  def new; end
+
+  def create
+    administrator = Administrator.find_by_email(params[:email])
+    if administrator&.authenticate(params[:password])
+      session[:administrator_id] = administrator.id
+      redirect_to root_path, notice: 'Logged in!'
+    else
+      flash.now.alert = 'Email or password is invalid'
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:administrator_id] = nil
+    redirect_to root_url, notice: 'Logged out!'
+  end
+end
